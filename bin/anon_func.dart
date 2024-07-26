808d676bf4d9bee0444d90fa3414f0825aa054b2
Han25 <farhaneditya03@gmail.com>
2024-07-26 18:06:39 +0700

    final commit basic

--- bin/anon_func.dart
+++ bin/anon_func.dart
@@ -1,1 +1,25 @@
-void main
+void hello(String name, String Function(String)filter){
+  var filteredName = filter(name);
+  print('Hi $filteredName');
+}
+
+void main(){
+//func as variable
+
+var toUpper = (String name) {
+  return name.toUpperCase();
+};
+
+var toLower = (String name) => name.toLowerCase();
+print(toUpper('han'));
+print(toLower('FARHAN'));
+
+//anon function
+hello('dongo', (name) {
+  return name == 'dongo' ? '****' : name;
+});
+
+//or you can meake it with short func
+hello('farhan', (String name)=> name == 'dongo' ? '****' : name);
+
+}
